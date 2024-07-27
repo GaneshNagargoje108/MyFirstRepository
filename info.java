adb2a4a3c3e082ea047c8ef561f26b869b1fcd20
GaneshNagargoje108 <ganeshnagargoje108@gmail.com>
2024-07-27 16:30:33 +0530

    Add files via upload

---
+++ info.java
@@ -0,0 +1,8 @@
+public class Information{
+	public static void main(String[] args){
+		System.out.println("Name: Ganesh Annarao Nagargoje");
+		System.out.println("class: TE");
+		System.out.println("course: Full stack java development");
+
+	}
+}
